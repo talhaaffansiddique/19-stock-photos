e892e572d53e9696d1f8323dd6afe06266727c27
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-11-29 12:54:07 +0500

    221 - Stock Photos Display Photos

--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    profile_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className="photo">
+      <img src={regular} alt={alt_description} />
+      <div className="photo-info">
+        <h4>{name}</h4>
+        <p>{likes} Likes</p>
+      </div>
+      <a href={profile_url}>
+        <img src={medium} alt={name} className="user-img" />
+      </a>
+    </article>
+  );
+};
 
-export default Photo
+export default Photo;
