0acb9c3add8e6219ebf85071860e41b13d7ccbfb
Talha Affan Siddique <talhaaffansiddique@gmail.com>
2023-11-30 12:23:55 +0500

    224 - Stock Photos Search Query

--- src/App.js
+++ src/App.js
@@ -11,17 +11,30 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
   const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
     const urlPage = `&page=${page}`;
-    url = `${mainUrl}${clientID}${urlPage}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos((oldPhoto) => {
-        return [...oldPhoto, ...data];
+      console.log(data);
+      setPhotos((oldPhotos) => {
+        if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
       });
       setLoading(false);
     } catch (error) {
@@ -50,15 +63,25 @@ function App() {
     });
     return () => window.removeEventListener("scroll", event);
   }, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    fetchImages();
   };
 
   return (
     <main>
       <section className="search">
         <form className="search-form">
-          <input type="text" placeholder="search" className="form-input" />
+          <input
+            type="text"
+            placeholder="search"
+            className="form-input"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
           <button type="submit" className="submit-btn" onClick={handleSubmit}>
             <FaSearch />
           </button>
